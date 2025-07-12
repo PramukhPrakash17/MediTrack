108893e1f09027f02b9143bcbfe7e440009b127c
Pramukh Prakash <pramukhp35@gmail.com>
2025-07-13 00:07:10 +0200

    feat(dependency)  updating dependency

--- Backend/src/main/java/com/pramukh/meditrack/Config/SecurityConfig.java
+++ Backend/src/main/java/com/pramukh/meditrack/Config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.pramukh.meditrack.Security;
+package com.pramukh.meditrack.Config;
 
 import com.pramukh.meditrack.Component.JwtFilter;
 import com.pramukh.meditrack.Service.CustomUserDetailsService;
@@ -7,11 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
